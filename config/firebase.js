e19edfd3db28db9571aba7deeda1dbb6a978b8a3
kazryll <kurtavryll7.nito@gmail.com>
2026-01-01 22:09:53 +0800

    fix: new firebase sdk

--- config/firebase.js
+++ config/firebase.js
@@ -5,14 +5,15 @@ import { getFirestore } from "firebase/firestore";
 
 // Web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBRV1JEt_qSWZPxpvouEUNzuPbW5gWW4yc",
+  apiKey: "AIzaSyBqVUfTUoxRuV5L5EJTRB-TX9knD33UvYk",
   authDomain: "ayos-lgu-web-app.firebaseapp.com",
   projectId: "ayos-lgu-web-app",
   storageBucket: "ayos-lgu-web-app.firebasestorage.app",
   messagingSenderId: "183096564899",
-  appId: "1:183096564899:web:edd2ecd570ed6bf719f316",
-  measurementId: "G-MWZ6TEV35K",
+  appId: "1:183096564899:web:6f2c38a5415e786519f316",
+  measurementId: "G-5TBPP3SXDN"
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
